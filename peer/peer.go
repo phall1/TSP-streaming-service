bd833a23b43b106facc6e450ef6476cad699a30f
phall <pshall95@gmail.com>
2018-04-24 01:03:43 -0700

    read files in dir. need to only reed mp3, not all

--- peer/peer.go
+++ peer/peer.go
@@ -5,36 +5,71 @@ import (
     "net"
     "fmt"
     "bufio"
+    "path"
+    "io/ioutil"
 )
 
-
 func main() {
-    args := os.Args[:]
-    if len(args) != 3 {
-        fmt.Println("Usage: ", args[0], "<port> <filedir>")
-        os.Exit(1)
-    }
+    args := check_args()
 
-    // Exit if filedir does not exist
-    if _, err := os.Stat(args[2]); os.IsNotExist(err) {
-        fmt.Println("filedir does not exist")
-        os.Exit(1)
-    }
+    songs := get_songs(args[2])
 
-    conn, err := net.Dial("tcp", "localhost:" + args[1])
+    // Connect to tacker
+    tracker, err := net.Dial("tcp", "localhost:" + args[1])
     if err != nil {
         fmt.Println("some error")
     }
 
+    send_song_list(tracker, songs)
+
     for {
+        
         reader := bufio.NewReader(os.Stdin)
         fmt.Println("text to send: ")
         text, _ := reader.ReadString('\n')
 
         // send to Socket
-        fmt.Fprintf(conn, text + "\n")
+        fmt.Fprintf(tracker, text + "\n")
 
-        reply, _ := bufio.NewReader(conn).ReadString('\n')
+        reply, _ := bufio.NewReader(tracker).ReadString('\n')
         fmt.Print("Message from server: " + reply)
     }
 }
+
+func check_args() []string{
+    args := os.Args[:]
+    if len(args) != 3 {
+        fmt.Println("Usage: ", args[0], "<port> <filedir>")
+        os.Exit(1)
+    }
+    return args
+}
+
+/*
+ * Returns an array of FileInfo types
+ * for all files(songs) in directory "songs"
+ */
+func get_songs(dir_name string) []os.FileInfo {
+
+    songs, err := ioutil.ReadDir(dir_name)
+    if err != nil {
+        fmt.Println("cant read songs")
+        os.Exit(1)
+    }
+
+    for _, f := range songs {
+        if path.Ext(f.Name()) != ".mp3" {
+            fmt.Println(f.Name())
+        }
+    }
+    return songs
+}
+
+/*
+ * send this peers song list to the 
+ * tracker server
+ */
+func send_song_list(tracker net.Conn, songs []os.FileInfo) {
+    
+}
+
