15de2d0c08b20ac53519f8feee754509cf85c39c
phall <pshall95@gmail.com>
2018-04-24 02:10:18 -0700

    remove check args function (put code in main), begin send local songs to tracker (func)

--- peer/peer.go
+++ peer/peer.go
@@ -10,9 +10,13 @@ import (
 )
 
 func main() {
-    args := check_args()
+    args := os.Args[:]
+    if len(args) != 3 {
+        fmt.Println("Usage: ", args[0], "<port> <filedir>")
+        os.Exit(1)
+    }
 
-    songs := get_songs(args[2])
+    songs := get_local_songs(args[2])
 
     // Connect to tacker
     tracker, err := net.Dial("tcp", "localhost:" + args[1])
@@ -20,10 +24,13 @@ func main() {
         fmt.Println("some error")
     }
 
-    send_song_list(tracker, songs)
+    send_local_songs(tracker, songs)
+
+    // receive remote song list
+
+    // wait for user action (play, other stuff)
 
     for {
-        
         reader := bufio.NewReader(os.Stdin)
         fmt.Println("text to send: ")
         text, _ := reader.ReadString('\n')
@@ -36,40 +43,37 @@ func main() {
     }
 }
 
-func check_args() []string{
-    args := os.Args[:]
-    if len(args) != 3 {
-        fmt.Println("Usage: ", args[0], "<port> <filedir>")
-        os.Exit(1)
-    }
-    return args
-}
-
 /*
- * Returns an array of FileInfo types
+ * Returns an string slice
  * for all files(songs) in directory "songs"
  */
-func get_songs(dir_name string) []os.FileInfo {
-
+// func get_songs(dir_name string) []os.FileInfo {
+func get_local_songs(dir_name string) []string {
     songs, err := ioutil.ReadDir(dir_name)
     if err != nil {
         fmt.Println("cant read songs")
         os.Exit(1)
     }
 
-    for _, f := range songs {
-        if path.Ext(f.Name()) != ".mp3" {
-            fmt.Println(f.Name())
+    songs_slice := make([]string, 0)
+    for i := 0; i < len(songs); i++ {
+        if path.Ext(songs[i].Name()) == ".mp3" {
+            songs_slice = append(songs_slice, songs[i].Name())
         }
     }
-    return songs
+    return songs_slice 
 }
 
 /*
  * send this peers song list to the 
  * tracker server
  */
-func send_song_list(tracker net.Conn, songs []os.FileInfo) {
-    
+// func send_song_list(tracker net.Conn, songs []os.FileInfo) {
+func send_local_songs(tracker net.Conn, songs []string) {
+    msg := ""
+    for _, s := range songs {
+        msg += s + "\n"
+    }
+    client.Write([]byte(msg))
 }
 
