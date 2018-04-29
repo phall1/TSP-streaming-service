bab61d3ed5e7decb83f212d2e5675452bbd668f8
phall <pshall95@gmail.com>
2018-04-29 13:38:51 -0700

    little fix to id, adding colon

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -126,7 +126,7 @@ func get_info_from_peer(peer net.Conn, song_bytes []byte) {
 		if s == "" {
 			continue
 		}
-		record := string(id_counter) + ip + s
+		record := string(id_counter) + ": " + ip + s
 		id_counter++
 		info = append(info, record)
 	}
